1c9d10e1a70fa1f5deae2116ad2fd84f59fdc83a
data-george <georgefrancispinto@gmail.com>
2025-02-24 16:25:16 -0500

    get emotion category values from text response and get category with highest value

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,4 +5,22 @@ def emotion_detector(text_to_analyse):  # Define a function named emotion_detect
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    return response.text  # Return the response text from the API
+    # Get the emotion scores
+    emotions = response.text["emotion"]
+    # Get each emotion category
+    anger_score = emotions["anger"]
+    disgust_score = emotions["disgust"]
+    fear_score = emotions["fear"]
+    joy_score = emotions["joy"]
+    sadness_score = emotions["sadness"]
+    # Get the emotion category with the highest value
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            'dominant_emotion': dominant_emotion
+           }
