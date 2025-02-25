17cf91ddd593e0b3efdda3ab5872eeaf25d3905b
data-george <georgefrancispinto@gmail.com>
2025-02-25 16:01:58 -0500

    incorporate error handling in emotion_detector and server scripts

--- server.py
+++ server.py
@@ -18,31 +18,19 @@ def sent_analyzer():
     # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyze)
 
-    # Check if status code is 400 return values for all keys as None
-    if response.status_code == 400:
-        anger = None
-        disgust = None
-        fear = None
-        joy = None
-        sadness = None
-        dominant_emotion = None 
-
-    # If status code is not 400 extract the emotion and score from the response
-    else:
-        anger = response['anger']
-        disgust = response['disgust']
-        fear = response['fear']
-        joy = response['joy']
-        sadness = response['sadness']
-        dominant_emotion = response['dominant_emotion']
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
 
     # Check if dominant emotion is None, if so display error message
     if dominant_emotion is None:
         return "Invalid text! Please try again!."
- 
     
+    # If dominant emotion is not None return a formatted string with the emotion label and score
     else:
-        # Return a formatted string with the emotion label and score
         return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
